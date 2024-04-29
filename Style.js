48b4df3db9e003cda5c5e5d9a43630ad6a146c5a
cindykimmm <158105086+cindykimmm@users.noreply.github.com>
2024-04-29 15:47:33 -0400

    Add files via upload

---
+++ Style.js
@@ -0,0 +1,23 @@
+let angle = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  noFill();
+}
+
+function draw() {
+  background(160);
+  
+  translate(width / 2, height / 2);
+  rotate(angle);
+
+  // rotating lines
+  for (let i = 0; i < 20; i++) {
+    let diameter = i * 20;
+    stroke(255 - i * 10, 0, 0); // colors
+    ellipse(0, 0, diameter, diameter);
+    line(0, -diameter / 2, 0, diameter / 2);
+  }
+
+  angle += 0.01; // speed
+}
